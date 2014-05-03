f12c22150d9cd512da94c4184448557f0ccd2429
Christopher Su <christophersu9@gmail.com>
2014-05-03 14:29:16 -0700

    working like all by user

---
+++ like_by_user.py
@@ -0,0 +1,17 @@
+# Like media by a certain user
+# by Christopher Su
+# May 3, 2014
+
+from instagram.client import InstagramAPI
+import secret
+
+MAX_NUM_ITEMS = 20 # the maximum number of items to like (starting from the most recent)
+USERNAME = 'boss_tweed'
+
+access_token = secret.ACCESS_TOKEN
+api = InstagramAPI(access_token=access_token)
+USER_ID = api.user_search(USERNAME, count=1)[0].id
+recent, next = api.user_recent_media(user_id=USER_ID, count=MAX_NUM_ITEMS)
+for cur in recent:
+    api.like_media(cur.id)
+    print "Liked %s" % cur.link
